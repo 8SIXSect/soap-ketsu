52f9b1d01838841e9df3cc2c6609c843e4f9f42f
8SIXSect <eightysixthsector@outlook.com>
2024-06-29 13:55:35 -0500

    Added assemble support for search-section

--- assemble.py
+++ assemble.py
@@ -13,11 +13,17 @@ import os
 import json
 import sys
 import re
-from typing import Dict, Any
+from typing import Dict, Any, List
 import jsmin
 
 
 SOURCE_DIRECTORY = "src"
+SOURCE_SUB_DIRECTORIES_TO_JSON_OBJECT_MAP: Dict[str, str] = {
+    "MainPage": "mainPage",
+    "Search": "search",
+    #"Info": "info",
+    #"Chapters": "chapters"
+}
 OUTPUT_INDENT_LEVEL = 4
 
 
@@ -36,28 +42,36 @@ if __name__ == "__main__":
         json_data: Dict[str, Any] = json.load(output_file_read_mode)
         print(f"Got JSON Data from -> {output_file_path}")
     
+    for sub_directory, json_key in SOURCE_SUB_DIRECTORIES_TO_JSON_OBJECT_MAP.items():
+        print("")
+
+        sub_directory_path: str = os.path.join(SOURCE_DIRECTORY, sub_directory)
+        
+        assert os.path.exists(sub_directory_path)
+        print(f"File path exists -> {sub_directory_path}")
+
+        files_in_sub_directory: List[str] = os.listdir(sub_directory_path)
+
+        assert len(files_in_sub_directory) > 0
+        print(f"Directory, {sub_directory_path}, is not empty")
+
+        javascript_file_path: str = os.path.join(sub_directory_path, files_in_sub_directory[0])
+        print(f"Found file at path -> {javascript_file_path}")
+        
+        with open(javascript_file_path, "r") as javascript_file:
+            file_contents: str = javascript_file.read()
+            print(f"Read Javascript File contents from -> {javascript_file_path}")
+
+        no_script_tag_pattern = r'<script\s+type="text/javascript">\s*|</script>'
+        no_script_tag_main_page: str = re.sub(no_script_tag_pattern, '', file_contents)
+        print(f"Removed Script Tags from -> {javascript_file_path}")
+
+        minified_javscript_code: str = jsmin.jsmin(no_script_tag_main_page) 
+        print(f"Minified Javscript at -> {javascript_file_path}")
+
+        json_data[json_key][0]["javascriptConfig"]["javaScript"] = minified_javscript_code
+
     print("")
-
-    main_page_path: str = os.path.join(SOURCE_DIRECTORY, "MainPage", "mainPage1.html")
-    assert os.path.exists(main_page_path)
-    print(f"File path exists -> {main_page_path}")
-
-    with open(main_page_path, "r") as main_page_file:
-        main_page_contents: str = main_page_file.read()
-        print(f"Read Main Page contents from -> {main_page_path}")
-
-    print("")
-
-    no_script_tag_pattern = r'<script\s+type="text/javascript">\s*|</script>'
-    no_script_tag_main_page: str = re.sub(no_script_tag_pattern, '', main_page_contents)
-    print("Removed Script Tags from Main Page")
-
-    minified_javscript_code: str = jsmin.jsmin(no_script_tag_main_page) 
-    print("Minified Javscript source code")
-
-    print("")
-
-    json_data["mainPage"][0]["javascriptConfig"]["javaScript"] = minified_javscript_code
 
     with open(output_file_path, "w") as output_file_write_mode:
         json.dump(json_data, output_file_write_mode, indent=OUTPUT_INDENT_LEVEL)
